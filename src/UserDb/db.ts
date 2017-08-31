53f81ae8e06a10ab098e3f6308a16fe52bd18d96
Yishai Zehavi <zehaviyishai@gmail.com>
2017-08-31 20:13:45 +0300

    Finished db wrapper

--- src/UserDb/db.ts
+++ src/UserDb/db.ts
@@ -1,4 +1,5 @@
 import { Core, Model, Instance, Collection, Index, Property, ObjectID } from 'iridium';
+import * as Bluebird from 'bluebird'
 
 interface DhtRecordDocument {
     _id?: string;
@@ -13,28 +14,46 @@ class DhtRecord extends Instance<DhtRecordDocument, DhtRecord> implements DhtRec
     @ObjectID _id: string;
     @Property(String) hash: string;
     @Property(Number) count: number;
-    @Property(String) files: string[];
+    @Property([String]) files: string[];
 }
 
 class UserDb extends Core {
     DhtRecords = new Model<DhtRecordDocument, DhtRecord>(this, DhtRecord);
 }
 
-let db = new UserDb({ database: 'newwa' });
+// Create instance of the mongodb
+const db = new UserDb({ database: 'newwa' });
 
-db.connect()
-    .then(() => {
-        db.DhtRecords.insert({
-            hash: '5dg9r2o0was3m',
-            count: 5,
-            files: [
-                'first_file',
-                'second_file',
-                'third_file'
-            ]
-        })
-    })
-    .then(() => db.DhtRecords.get())
-    .then(record => console.log(record))
-    .then(() => db.close())
-    .catch(err => console.error(err.message));
+
+export const connect = (): Bluebird<UserDb | Error> => {
+    return db.connect()
+        .then(() => db)
+        .catch(err => new Error('Couldn\'t connect to mongodb'));
+};
+
+export const save = (hash: string, count: number, files: string[]): Bluebird<Object> => {
+    return db.DhtRecords
+        .insert({ hash, count, files })
+        .then(() => { return { ok: true } })
+        .catch(() => { return { ok: false } });
+};
+
+export const get = (hash: number): Bluebird<DhtRecord | Error> => {
+    return db.DhtRecords
+        .get({ hash })
+        .then((record: DhtRecord) => record)
+        .catch(err => new Error('Couldn\'t find the specified hash'));
+};
+
+export const close = (): Bluebird<Object> => {
+    return db.close()
+        .then(() => { return { ok: true } })
+        .catch(err => { return { ok: false } });
+};
+
+export default {
+    connect,
+    save,
+    get,
+    close
+};
